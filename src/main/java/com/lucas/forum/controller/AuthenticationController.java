611f435aa811aad272f182417ad361fedb2bf18c
lucasgcaldas <lggomes007@gmail.com>
2022-02-22 11:52:51 -0300

    Adding AuthenticationController class

---
+++ src/main/java/com/lucas/forum/controller/AuthenticationController.java
@@ -0,0 +1,41 @@
+package com.lucas.forum.controller;
+
+import com.lucas.forum.config.security.TokenService;
+import com.lucas.forum.controller.form.LoginForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity<?> authentic(@RequestBody @Valid LoginForm form) {
+        UsernamePasswordAuthenticationToken loginData = form.convert();
+
+        try {
+            Authentication authenticate = authenticationManager.authenticate(loginData);
+            String token = tokenService.generateToken(authenticate);
+
+            return ResponseEntity.ok().build();
+        }catch (AuthenticationException e){
+            return ResponseEntity.badRequest().buid();
+        }
+    }
+}
